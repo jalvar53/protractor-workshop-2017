372856bb1cd001a3c360baa38d13bd021a6d163e
Jose Luis Alvarez Herrera <jalvar53@eafit.edu.co>
2017-12-05 13:52:22 -0500

    Deactivating implicit wait and fixing errors with explicit wait (#12)
    * Deactivating implicit wait and fixing errors with explicit wait
    
    * Fixed to make the waiting asynchronous
    
    * Adding a default timeout value for the explicit wait

--- src/page/ProductAddedModal.page.ts
+++ src/page/ProductAddedModal.page.ts
@@ -1,11 +1,12 @@
-import { $, ElementFinder, promise } from 'protractor';
+import { $, ElementFinder, promise, browser, ExpectedConditions } from 'protractor';
 
 export class ProductAddedModalPage {
   private get productAddedModal(): ElementFinder {
     return $('[style*="display: block;"] .button-container > a');
   }
 
-  public goToSummary(): promise.Promise<void> {
+  public async goToSummary(): Promise<void> {
+    await browser.wait(ExpectedConditions.elementToBeClickable(this.productAddedModal), 4000);
     return this.productAddedModal.click();
   }
 }

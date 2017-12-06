ccc9d16471daae2edfcf32829ddb98a0f3c8e256
Jose Luis Alvarez Herrera <jalvar53@eafit.edu.co>
2017-12-06 11:28:43 -0500

    Removing Popup messages and infobars (#16)

--- protractor/config.ts
+++ protractor/config.ts
@@ -11,6 +11,13 @@ export const config: Config = {
   SELENIUM_PROMISE_MANAGER: false,
   specs: ['../test/*.spec.js'],
   noGlobals: true,
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['disable-infobars=true --windows-size=800,600'],
+      prefs: { credentials_enable_service: false }
+    }
+  },
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     browser.manage().timeouts().implicitlyWait(0);

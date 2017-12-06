a1c24f17cb5ae042ee7e55bc4ebbf92af4b47f81
Jose Luis <jalvar53@eafit.edu.co>
2017-12-06 08:31:05 -0500

    Adding describes to each step of the test

--- test/buyTshirt.spec.ts
+++ test/buyTshirt.spec.ts
@@ -25,22 +25,31 @@ describe('Buy a t-shirt', () => {
  const bankPaymentStepPage: BankPaymentStepPage = new BankPaymentStepPage();
  const orderResumePage: OrderResumePage = new OrderResumePage();
 
- it('then should be bought a t-shirt', async () => {
-    await browser.get('http://automationpractice.com/');
+ describe('Given a clothing website', async () => {
+  await browser.get('http://automationpractice.com/');
+  describe('When the user chooses a T-shirt from the catalog', async () => {
     await menuContentPage.goToShirtMenu();
     await productListPage.goToShirtDetailsMenu();
     await productDetailPage.goToCartMenu();
     await productAddedModalPage.goToSummary();
     await summaryStepPage.goToSiginMenu();
-    await signInStepPage.fillSignInForm('aperdomobo@gmail.com', 'WorkshopProtractor');
-    await signInStepPage.goToAddressesMenu();
-    await addressStepPage.goToShippingMenu();
-    await shippingStepPage.agreeTermsOfService();
-    await shippingStepPage.goToPaymentMenu();
-    await paymentStepPage.goToBankPaymentMenu();
-    await bankPaymentStepPage.goToOrderMenu();
- 
-    await expect(orderResumePage.getOrderStatus.getText())
-      .toBe('Your order on My Store is complete.');
+    describe('And logs in with his account', async () => {
+      await signInStepPage.fillSignInForm('aperdomobo@gmail.com', 'WorkshopProtractor');
+      await signInStepPage.goToAddressesMenu();
+      await addressStepPage.goToShippingMenu();
+      describe('And chooses his default address', () => {
+        await shippingStepPage.agreeTermsOfService();
+        await shippingStepPage.goToPaymentMenu();
+        describe('And pays for his order', () => {
+          await paymentStepPage.goToBankPaymentMenu();
+          await bankPaymentStepPage.goToOrderMenu();
+          it('Then, the order should be correct and completed', () => {
+            await expect(orderResumePage.getOrderStatus.getText())
+            .toBe('Your order on My Store is complete.');
+          });
+        });
+      });
+    });
   });
  });
+});

9bcfaaf3fcb6aeebb0875489880ad3c5a40ea238
Jose Luis <jalvar53@eafit.edu.co>
2017-12-06 07:45:00 -0500

    Little improvements

--- src/page/ProductList.page.ts
+++ src/page/ProductList.page.ts
@@ -2,7 +2,7 @@ import { $, ElementFinder, promise } from 'protractor';
 
 export class ProductListPage {
   private get tShirtImage(): ElementFinder {
-    return $('[alt*="Faded"]');
+    return $('.product_img_link > img');
   }
 
   public goToShirtDetailsMenu(): promise.Promise<void> {

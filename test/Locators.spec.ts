a53a7b6802e17586c2e73f68cd5be4f2b0e016c5
Jose Luis <jalvarezh@psl.com.co>
2017-12-11 10:36:43 -0500

    Awaiting the values, not the expect

--- test/Locators.spec.ts
+++ test/Locators.spec.ts
@@ -27,17 +27,17 @@ describe('Fill form', () => {
 
         describe('When the user fills every input', () => {
             beforeAll(async () => {
-                await personalInformationPage.fillForm(formInformation);
+                personalInformationPage.fillForm(formInformation);
             });
 
             it('The title should be "Practice Automation Form"', async () => {
-                await expect(personalInformationPage.title.getText()).toBe("Practice Automation Form");
+                expect(await personalInformationPage.title.getText()).toBe("Practice Automation Form");
             });
 
             it('The image should be loaded', async () => {
                 const pathIndex = formInformation.file.lastIndexOf('\\');
                 const filename = formInformation.file.substring(pathIndex);
-                await expect(personalInformationPage.getImageName()).toMatch(filename);
+                expect(await personalInformationPage.getImageName()).toMatch(filename);
             });
         });
     });
